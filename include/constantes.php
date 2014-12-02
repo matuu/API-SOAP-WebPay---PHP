96fff09e8967d79f75ff29366e28798762c388d2
DannielWhatever <DannielGutierrez90@gmail.com>
2014-12-01 22:27:52 -0300

    eliminar lo que se me paso :/

--- include/constantes.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-define("PRIVATE_KEY",dirname(__FILE__)."/keys/oneclick.key");
-define("CERT_FILE",dirname(__FILE__)."/keys/oneclick.crt");
-define("SERVER_CERT",dirname(__FILE__)."/keys/serverDesaTbk.crt");
-define("MSJ_ERROR_WS","Error al realizar petici&oacute;n al Servicio Web.");
-
-define("SITIO_CERT","webpay.desa/tienda/webpay");  #
-define("COMMERCE_CODE",597020000093); #código de comercio
